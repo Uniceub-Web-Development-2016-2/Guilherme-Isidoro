6fec3eb19746f65abb4d666230fe2aeffd8aeafc
GuilhermeIsidoro <guilhermeisidoro@ymail.com>
2016-10-23 20:00:49 -0200

    Updating API Get's Put's and Post's

---
+++ CMO/404.php
@@ -0,0 +1,3 @@
+<?php
+
+echo "Essa pagina nao existe";
---
+++ CMO/user-ID.php
@@ -0,0 +1,51 @@
+<?php
+include('control/request_controller.php');
+
+$controller = new RequestController();
+
+$dados = $controller->execute();
+
+$arr = utf8_converter($dados);
+
+$routesJson = json_encode($arr); //Retorno especificado na API
+
+function utf8_converter($array)
+{
+    array_walk_recursive($array, function(&$item, $key){
+        if(!mb_detect_encoding($item, 'utf-8', true)){
+                $item = utf8_encode($item);
+        }
+    });
+ 
+    return $array;
+}
+
+foreach($arr as $item) { 
+    echo '<br>'.'Nome: '.$item['name'].' ID: '.$item['id'].' Cidade: '.$item['city_ID'].'<br>'; 
+}
+
+
+switch (json_last_error()) {
+        case JSON_ERROR_NONE:
+            echo ' - No errors';
+        break;
+        case JSON_ERROR_DEPTH:
+            echo ' - Maximum stack depth exceeded';
+        break;
+        case JSON_ERROR_STATE_MISMATCH:
+            echo ' - Underflow or the modes mismatch';
+        break;
+        case JSON_ERROR_CTRL_CHAR:
+            echo ' - Unexpected control character found';
+        break;
+        case JSON_ERROR_SYNTAX:
+            echo ' - Syntax error, malformed JSON';
+        break;
+        case JSON_ERROR_UTF8:
+            echo ' - Malformed UTF-8 characters, possibly incorrectly encoded';
+        break;
+        default:
+            echo ' - Unknown error';
+        break;
+    }
+
